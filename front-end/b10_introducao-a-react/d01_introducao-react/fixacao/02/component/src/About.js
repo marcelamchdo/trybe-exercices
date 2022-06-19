374090790e796032c96420dd4c848baa28436676
marcela <arqmrcl@gmail.com>
2022-06-19 15:53:38 -0300

    aaaaa

---
+++ front-end/b10_introducao-a-react/d01_introducao-react/fixacao/02/component/src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class About extends React.Component {
+    const name = 'Marcela Machado'
+    const describe = 'Lorem laborum consequat officia ut Lorem ea labore est ullamco non veniam veniam irure elit. Officia velit anim cupidatat ut anim eu cupidatat voluptate laboris do sint fugiat ea. Eiusmod commodo adipisicing excepteur proident anim enim ad laborum ad velit ullamco. Deserunt laborum Lorem exercitation dolor laboris in. Voluptate esse aute duis magna aliquip voluptate laboris incididunt cupidatat esse aliquip nostrud.Pariatur amet dolore proident sint. Anim ea labore nostrud mollit dolor aute nisi velit Lorem et dolor Lorem anim. Ipsum occaecat exercitation eiusmod anim voluptate laboris id cillum ex proident proident.Aute aliquip dolore magna quis proident mollit duis. Excepteur ad aliqua irure esse nostrud sit ullamco ut anim. Ullamco non amet do cillum adipisicing.Sit nisi eiusmod commodo Lorem sunt ut proident id anim dolore laboris et velit ad. Reprehenderit excepteur enim in quis elit reprehenderit fugiat culpa. Do elit in ut elit eu consequat occaecat laborum exercitation incididunt eu.Enim ad Lorem eiusmod labore sit. Dolor consectetur aute nisi reprehenderit minim ad non eu nulla ex cupidatat ea cillum. Non aliquip sunt enim Lorem. Duis ea et dolore culpa incididunt voluptate excepteur laboris eu nostrud non. Duis duis ex non ex qui dolor. Incididunt adipisicing non deserunt aute tempor quis commodo.'
+    return (
+        <h1> {name} </h1>
+        
+    )
+}
+
+export default About;
